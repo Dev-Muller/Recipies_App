df5ad8039ec94ca2892b3dd0ba326d869c5ced5c
Kevin-Amaral165 <kevin_ramos1@hotmail.com>
2023-05-06 13:57:23 -0300

    feat: impleneta funcionalidades e botoes do profile, requisitos 57,58,59

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from '../components/header/Header';
 
 function Profile() {
+  const [email, setEmail] = useState('');
+
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem('user'));
+    if (user && user.email) {
+      setEmail(user.email);
+    }
+  }, []);
   return (
-    <Header
-      title="Profile"
-    />
+    <>
+      <Header title="Profile" />
+      <div>
+        <p data-testid="profile-email">
+          {email}
+        </p>
+        <button data-testid="profile-done-btn">Done Recipes</button>
+        <button data-testid="profile-favorite-btn">Favorite Recipes</button>
+        <button data-testid="profile-logout-btn">Logout</button>
+      </div>
+    </>
   );
 }
 
