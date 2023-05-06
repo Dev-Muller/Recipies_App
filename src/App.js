5019208cfd94960f36c3cc42480736fef6097c00
lucopdev <lucopdev@gmail.com>
2023-05-05 22:31:24 -0300

    feat: criando o componente footer

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Route, Switch } from 'react-router-dom';
 import Login from './pages/Login';
+import Footer from './components/footer/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route exact path="/" component={ Login } />
         <Route path="/meals" component="" />
       </Switch>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import drinkIcon from '../../images/drinkIcon.svg';
+import mealIcon from '../../images/mealIcon.svg';
+import '../../styles/footer.css';
+
+function Footer() {
+  return (
+    <footer data-testid="footer">
+      <Link to="drinks">
+        <img
+          data-testid="drinks-bottom-btn"
+          src={ drinkIcon }
+          alt="drikns-btn"
+        />
+      </Link>
+      <Link to="/meals">
+        <img
+          data-testid="meals-bottom-btn"
+          src={ mealIcon }
+          alt="meal-btn"
+        />
+      </Link>
+    </footer>
+  );
+}
+
+export default Footer;
