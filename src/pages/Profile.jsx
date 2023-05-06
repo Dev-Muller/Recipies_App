04b6a2cb16ffe8a979d604296082e3c1b9322edc
Kevin-Amaral165 <kevin_ramos1@hotmail.com>
2023-05-06 14:30:57 -0300

    feat:redireciona pagina de login e limpa localstorage, falta testes, requisito 62

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import Header from '../components/header/Header';
 
 function Profile() {
   const [email, setEmail] = useState('');
+  const history = useHistory();
 
   useEffect(() => {
     const user = JSON.parse(localStorage.getItem('user'));
@@ -12,20 +13,32 @@ function Profile() {
     }
   }, []);
 
+  const handleLogout = () => {
+    localStorage.clear();
+    history.push('/');
+  };
+
   return (
     <>
       <Header title="Profile" />
       <div>
+
         <p data-testid="profile-email">
           {email}
         </p>
+
         <button data-testid="profile-done-btn">
           <Link to="/done-recipes">Done Recipes</Link>
         </button>
+
         <button data-testid="profile-favorite-btn">
           <Link to="/favorite-recipes">Favorite Recipes</Link>
         </button>
-        <button data-testid="profile-logout-btn">Logout</button>
+
+        <button data-testid="profile-logout-btn" onClick={ handleLogout }>
+          Logout
+        </button>
+
       </div>
     </>
   );
