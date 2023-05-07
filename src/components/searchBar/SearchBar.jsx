c0b95f5a46dd4f39770e10c2e9292009aff9507c
lucopdev <lucopdev@gmail.com>
2023-05-06 22:40:58 -0300

    feat: fazer a requisição da api de meals and drinks para a barra de pesquisa  - requisito 11 e 12

--- src/components/searchBar/SearchBar.jsx
+++ src/components/searchBar/SearchBar.jsx
@@ -1,20 +1,73 @@
-import React from 'react';
+import React, { useContext, useState } from 'react';
 import './searchBar.css';
+import { useHistory } from 'react-router-dom';
+import AppContext from '../../context/AppContext';
 
 function SearchBar() {
+  const { fetchMealsApi, fetchDrinksApi } = useContext(AppContext);
+  const [searchInput, setSearchInput] = useState('');
+  const [radioBtn, setRadioBtn] = useState('');
+  const history = useHistory();
+
+  const handleClick = (radio, searchElement) => {
+    if (radio === 'first-letter' && searchInput.length > 1) {
+      global.alert('Your search must have only 1 (one) character');
+    }
+    if (history.location.pathname === '/meals') {
+      fetchMealsApi(radio, searchElement);
+    }
+    if (history.location.pathname === '/drinks') {
+      fetchDrinksApi(radio, searchElement);
+    }
+  };
+
   return (
     <div className="search-bar-container">
       <input
+        name="searchInput"
         className="input-search"
         data-testid="search-input"
         type="text"
+        onChange={ ({ target }) => setSearchInput(target.value) }
       />
       <div className="radios">
-        <input data-testid="ingredient-search-radio" type="radio" />
-        <input data-testid="name-search-radio" type="radio" />
-        <input data-testid="first-letter-search-radio" type="radio" />
+        <label>
+          <input
+            name="radioBtn"
+            value="ingredient"
+            data-testid="ingredient-search-radio"
+            type="radio"
+            onChange={ ({ target }) => setRadioBtn(target.value) }
+          />
+          Ingredient
+        </label>
+        <label>
+          <input
+            name="radioBtn"
+            value="name"
+            data-testid="name-search-radio"
+            type="radio"
+            onChange={ ({ target }) => setRadioBtn(target.value) }
+          />
+          Name
+        </label>
+        <label>
+          <input
+            name="radioBtn"
+            value="first-letter"
+            data-testid="first-letter-search-radio"
+            type="radio"
+            onChange={ ({ target }) => setRadioBtn(target.value) }
+          />
+          First letter
+        </label>
       </div>
-      <button data-testid="exec-search-btn">Search</button>
+      <button
+        data-testid="exec-search-btn"
+        onClick={ () => handleClick(radioBtn, searchInput) }
+      >
+        Search
+      </button>
     </div>
   );
 }
--- src/context/AppProvider.js
+++ src/context/AppProvider.js
@@ -7,11 +7,55 @@ function AppProvider({ children }) {
     email: '',
     password: '',
   });
+  const [apiData, setApiData] = useState([]);
+
+  const fetchMealsApi = async (radio, searchElement) => {
+    let URL = '';
+    switch (radio) {
+    case 'ingredient':
+      URL = `https://www.themealdb.com/api/json/v1/1/filter.php?i=${searchElement}`;
+      break;
+    case 'name':
+      URL = `https://www.themealdb.com/api/json/v1/1/search.php?s=${searchElement}`;
+      break;
+    case 'first-letter':
+      URL = `https://www.themealdb.com/api/json/v1/1/search.php?f=${searchElement}`;
+      break;
+    default:
+      break;
+    }
+    const response = await fetch(URL);
+    const result = await response.json();
+    setApiData(result.meals);
+  };
+
+  const fetchDrinksApi = async (radio, searchElement) => {
+    let URL = '';
+    switch (radio) {
+    case 'ingredient':
+      URL = `https://www.thecocktaildb.com/api/json/v1/1/filter.php?i=${searchElement}`;
+      break;
+    case 'name':
+      URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${searchElement}`;
+      break;
+    case 'first-letter':
+      URL = `https://www.thecocktaildb.com/api/json/v1/1/search.php?f=${searchElement}`;
+      break;
+    default:
+      break;
+    }
+    const response = await fetch(URL);
+    const result = await response.json();
+    setApiData(result.drinks);
+  };
 
   const values = useMemo(() => ({
+    apiData,
     login,
     setLogin,
-  }), [login]);
+    fetchMealsApi,
+    fetchDrinksApi,
+  }), [login, apiData]);
 
   return (
     <AppContext.Provider value={ values }>
