03fd6fa6471bec4e38a6c795df1ccb7e41ab667c
lucopdev <lucopdev@gmail.com>
2023-05-08 12:43:26 -0300

    feats: adicionado botões de filtro de receitas

--- src/pages/doneRecipes/DoneRecipes.jsx
+++ src/pages/doneRecipes/DoneRecipes.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Header from '../../components/header/Header';
-import Footer from '../../components/footer/Footer';
 
 function DoneRecipes() {
   return (
     <div>
       <Header title="Done Recipes" />
-      <Footer />
     </div>
   );
 }
--- src/pages/favoriteRecipes/FavoriteRecipes.jsx
+++ src/pages/favoriteRecipes/FavoriteRecipes.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import Header from '../../components/header/Header';
-import Footer from '../../components/footer/Footer';
 
 function FavoriteRecipes() {
   return (
     <div>
       <Header title="Favorite Recipes" />
-      <Footer />
     </div>
   );
 }
