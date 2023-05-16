b95164310212cae4a249ed14d8ee5c66d7ca2d82
lucopdev <lucopdev@gmail.com>
2023-05-16 11:39:04 -0300

    feats: requisito 42 e 43

--- src/components/startRecipeButton/StartRecipeButton.jsx
+++ src/components/startRecipeButton/StartRecipeButton.jsx
@@ -129,5 +129,5 @@ function StartRecipeButton({ handleDoneRecipes }) {
 StartRecipeButton.propTypes = {
   handleDoneRecipes: PropTypes.func.isRequired,
 };
-
+// mod
 export default StartRecipeButton;
