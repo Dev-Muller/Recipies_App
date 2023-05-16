41abf0a88bb4d2ba7f51c4fd2759e37736f0ab81
Vinicius Leme <vinileme@gmail.com>
2023-05-16 09:05:23 -0300

    feat: testes da pagina Done Recipes

--- src/tests/DoneRecipes.test.js
+++ src/tests/DoneRecipes.test.js
@@ -1,192 +1,175 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen, render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
-import { act } from 'react-dom/test-utils';
 import App from '../App';
-import AppProvider from '../context/AppProvider';
-import fetch from '../../cypress/mocks/fetch';
 
-describe('Testes component DoneRecipes', () => {
-  beforeEach(() => {
-    jest.spyOn(global, 'fetch').mockImplementation(fetch);
-  });
+describe('Testa as funcionalidadea da página Done Recipes', () => {
+  const setLocalStorage = (key, data) => {
+    window.localStorage.setItem(key, JSON.stringify(data));
+  };
 
-  it('Testa se há os botões de filtros em Tela', () => {
-    const history = createMemoryHistory();
-    render(
-      <Router history={ history }>
-        <AppProvider>
-          <App />
-        </AppProvider>
-      </Router>,
-    );
-    act(() => {
-      // eslint-disable-next-line sonarjs/no-duplicate-string
-      history.push('/done-recipes');
+  const renderWithRouter = (component, initialRoute = '/') => {
+    const history = createMemoryHistory({ initialEntries: [initialRoute] });
+    return ({
+      ...render(<Router history={ history }>{component}</Router>), history,
     });
-    const allBtn = screen.getByTestId('filter-by-all-btn');
-    const foodBtn = screen.getByTestId('filter-by-food-btn');
-    const drinkBtn = screen.getByTestId('filter-by-drink-btn');
-    expect(allBtn).toBeInTheDocument();
-    expect(foodBtn).toBeInTheDocument();
-    expect(drinkBtn).toBeInTheDocument();
-  });
+  };
 
+  const recipes = [
+    {
+      alcoholicOrNot: '',
+      nationality: 'Turkish',
+      category: 'Side',
+      doneDate: '2023-03-07T18:38:29.862Z',
+      image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg',
+      name: 'Corba',
+      tags: [
+        'Soup',
+      ],
+      type: 'meal',
+      id: '52977',
+    },
+    {
+      alcoholicOrNot: '',
+      nationality: 'Japanese',
+      category: 'Seafood',
+      doneDate: '2023-03-07T18:39:08.611Z',
+      image: 'https://www.themealdb.com/images/media/meals/g046bb1663960946.jpg',
+      name: 'Sushi',
+      tags: [],
+      type: 'meal',
+      id: '53065',
+
+    },
+    {
+      alcoholicOrNot: 'Alcoholic',
+      nationality: '',
+      category: 'Cocktail',
+      doneDate: '2023-03-07T18:39:42.057Z',
+      image: 'https://www.thecocktaildb.com/images/media/drink/2x8thr1504816928.jpg',
+      name: 'A1',
+      tags: [],
+      type: 'drink',
+      id: '17222',
+    },
+  ];
+
+  const route = '/done-recipes';
+  const allIdBtn = 'filter-by-all-btn';
+  const mealsIdBtn = 'filter-by-meal-btn';
+  const drinksIdBtn = 'filter-by-drink-btn';
+  setLocalStorage('doneRecipes', recipes);
+
+  it('testa se há os botões de filtros em Tela', () => {
+    renderWithRouter(<App />, route);
+    const buttonAll = screen.getByTestId(allIdBtn);
+    const buttonMeals = screen.getByTestId(mealsIdBtn);
+    const buttonDrinks = screen.getByTestId(drinksIdBtn);
+    expect(buttonAll).toBeInTheDocument();
+    expect(buttonMeals).toBeInTheDocument();
+    expect(buttonDrinks).toBeInTheDocument();
+  });
   it('Testa se as as receitas salvas no local storage são renderizadas corretamente.', () => {
-    const history = createMemoryHistory();
-    render(
-      <Router history={ history }>
-        <AppProvider>
-          <App />
-        </AppProvider>
-      </Router>,
-    );
-    act(() => {
-      history.push('/done-recipes');
-    });
-    const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
-    const doneRecipesLength = doneRecipes.length;
-    // eslint-disable-next-line sonarjs/no-duplicate-string
-    const doneRecipesCards = screen.getAllByTestId('recipe-card');
-    expect(doneRecipesCards.length).toBe(doneRecipesLength);
+    renderWithRouter(<App />, route);
+    const recipe1 = screen.getByText('Corba');
+    const recipe2 = screen.getByText('Sushi');
+    const recipe3 = screen.getByText('A1');
+    expect(recipe1).toBeInTheDocument();
+    expect(recipe2).toBeInTheDocument();
+    expect(recipe3).toBeInTheDocument();
   });
 
   it('Testa o botão de filtro Meals.', () => {
-    const history = createMemoryHistory();
-    render(
-      <Router history={ history }>
-        <AppProvider>
-          <App />
-        </AppProvider>
-      </Router>,
-    );
-    act(() => {
-      history.push('/done-recipes');
-    });
-    const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
-    const doneRecipesLength = doneRecipes.length;
-    const doneRecipesCards = screen.getAllByTestId('recipe-card');
-    expect(doneRecipesCards.length).toBe(doneRecipesLength);
-    const foodBtn = screen.getByTestId('filter-by-food-btn');
-    userEvent.click(foodBtn);
-    const doneRecipesCardsFood = screen.getAllByTestId('recipe-card');
-    const doneRecipesFood = doneRecipes.filter((recipe) => recipe.type === 'comida');
-    expect(doneRecipesCardsFood.length).toBe(doneRecipesFood.length);
+    renderWithRouter(<App />, route);
+    const recipe1 = screen.getByText('Corba');
+    const recipe2 = screen.getByText('Sushi');
+    const recipe3 = screen.getByText('A1');
+    expect(recipe1).toBeInTheDocument();
+    expect(recipe2).toBeInTheDocument();
+    expect(recipe3).toBeInTheDocument();
+    const buttonMeals = screen.getByTestId(mealsIdBtn);
+    userEvent.click(buttonMeals);
+    expect(recipe1).toBeInTheDocument();
+    expect(recipe2).toBeInTheDocument();
+    expect(recipe3).not.toBeInTheDocument();
   });
 
   it('Testa o botão de filtro Drinks.', () => {
-    const history = createMemoryHistory();
-    render(
-      <Router history={ history }>
-        <AppProvider>
-          <App />
-        </AppProvider>
-      </Router>,
-    );
-    act(() => {
-      history.push('/done-recipes');
-    });
-    const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
-    const doneRecipesLength = doneRecipes.length;
-    const doneRecipesCards = screen.getAllByTestId('recipe-card');
-    expect(doneRecipesCards.length).toBe(doneRecipesLength);
-    const drinkBtn = screen.getByTestId('filter-by-drink-btn');
-    userEvent.click(drinkBtn);
-    const doneRecipesCardsDrink = screen.getAllByTestId('recipe-card');
-    const doneRecipesDrink = doneRecipes.filter((recipe) => recipe.type === 'bebida');
-    expect(doneRecipesCardsDrink.length).toBe(doneRecipesDrink.length);
+    renderWithRouter(<App />, route);
+    const recipe1 = screen.getByText('Corba');
+    const recipe2 = screen.getByText('Sushi');
+    const recipe3 = screen.getByText('A1');
+    expect(recipe1).toBeInTheDocument();
+    expect(recipe2).toBeInTheDocument();
+    expect(recipe3).toBeInTheDocument();
+    const buttonDrinks = screen.getByTestId(drinksIdBtn);
+    userEvent.click(buttonDrinks);
+    expect(recipe1).not.toBeInTheDocument();
+    expect(recipe2).not.toBeInTheDocument();
+    expect(recipe3).toBeInTheDocument();
   });
 
   it('Testa o botão de filtro All.', () => {
-    const history = createMemoryHistory();
-    render(
-      <Router history={ history }>
-        <AppProvider>
-          <App />
-        </AppProvider>
-      </Router>,
-    );
-    act(() => {
-      history.push('/done-recipes');
-    });
-    const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
-    const doneRecipesLength = doneRecipes.length;
-    const doneRecipesCards = screen.getAllByTestId('recipe-card');
-    expect(doneRecipesCards.length).toBe(doneRecipesLength);
-    const allBtn = screen.getByTestId('filter-by-all-btn');
-    userEvent.click(allBtn);
-    const doneRecipesCardsAll = screen.getAllByTestId('recipe-card');
-    expect(doneRecipesCardsAll.length).toBe(doneRecipesLength);
+    renderWithRouter(<App />, route);
+    const recipe1 = screen.getByText('Corba');
+    const recipe2 = screen.getByText('Sushi');
+    const recipe3 = screen.getByText('A1');
+    expect(recipe1).toBeInTheDocument();
+    expect(recipe2).toBeInTheDocument();
+    expect(recipe3).toBeInTheDocument();
+    const buttonAll = screen.getByTestId(allIdBtn);
+    userEvent.click(buttonAll);
+    expect(recipe1).toBeInTheDocument();
+    expect(recipe2).toBeInTheDocument();
+    expect(recipe3).toBeInTheDocument();
   });
 
-  it('Testa se ao clicar no card de uma receita, o usuário é redirecionado para a tela de detalhes da receita.', async () => {
-    const history = createMemoryHistory();
-    const { findByTestId } = render(
-      <Router history={ history }>
-        <AppProvider>
-          <App />
-        </AppProvider>
-      </Router>,
-    );
-    act(() => {
-      history.push('/done-recipes');
-    });
-    const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
-    const doneRecipesLength = doneRecipes.length;
-    const doneRecipesCards = await screen.findAllByTestId('recipe-card');
-    expect(doneRecipesCards.length).toBe(doneRecipesLength);
-    const recipeCard = await findByTestId('0-horizontal-image');
-    userEvent.click(recipeCard);
-    const { pathname } = history.location;
-    expect(pathname).toBe('/comidas/52771');
+  it('Testa se ao clicar em uma receita, é redirecionado para a tela de detalhes.', () => {
+    renderWithRouter(<App />, route);
+    const recipe1 = screen.getByText('Corba');
+    expect(recipe1).toBeInTheDocument();
+    userEvent.click(recipe1);
+    const { pathname } = window.location;
+    expect(pathname).toBe('/comidas/52977');
   });
 
-  it('Testa se ao clicar no botão de compartilhar, o link da receita é copiado para o clipboard.', async () => {
-    const history = createMemoryHistory();
-
-    const { findByTestId } = render(
-      <Router history={ history }>
-        <AppProvider>
-          <App />
-        </AppProvider>
-      </Router>,
-    );
-    act(() => {
-      history.push('/done-recipes');
-    });
-    const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
-    const doneRecipesLength = doneRecipes.length;
-    const doneRecipesCards = await screen.findAllByTestId('recipe-card');
-    expect(doneRecipesCards.length).toBe(doneRecipesLength);
-    const recipeCard = await findByTestId('0-horizontal-share-btn');
-    userEvent.click(recipeCard);
-    const { pathname } = history.location;
-    expect(pathname).toBe('/comidas/52771');
+  it('Testa se ao não clicar em nenhum filtro e clicar em uma receita, é redirecionado para a tela de detalhes.', () => {
+    renderWithRouter(<App />, route);
+    const recipe1 = screen.getByText('Corba');
+    expect(recipe1).toBeInTheDocument();
+    userEvent.click(recipe1);
+    const { pathname } = window.location;
+    expect(pathname).toBe('/comidas/52977');
   });
 
-  it('Testa se ao clicar no botão de favoritar, a receita é removida da tela.', async () => {
-    const history = createMemoryHistory();
+  it('Testa o botão de Compartilhar', () => {
+    renderWithRouter(<App />, route);
+    const recipe1 = screen.getByText('Corba');
+    expect(recipe1).toBeInTheDocument();
+    userEvent.click(recipe1);
+    const shareBtn = screen.getByTestId('share-btn');
+    userEvent.click(shareBtn);
+    const link = screen.getByTestId('link');
+    expect(link).toBeInTheDocument();
+    expect(link.value).toBe('http://localhost:3000/comidas/52977');
+  });
 
-    const { findByTestId, queryByTestId } = render(
-      <Router history={ history }>
-        <AppProvider>
-          <App />
-        </AppProvider>
-      </Router>,
-    );
+  it('Testa o botão de Favoritar', () => {
+    renderWithRouter(<App />, route);
+    const recipe1 = screen.getByText('Corba');
+    expect(recipe1).toBeInTheDocument();
+    userEvent.click(recipe1);
+    const favoriteBtn = screen.getByTestId('favorite-btn');
+    userEvent.click(favoriteBtn);
+    const favoriteRecipes = JSON.parse(localStorage.getItem('favoriteRecipes'));
+    expect(favoriteRecipes).toEqual([{ id: '52977', type: 'comida', area: 'Turkish', category: 'Side', alcoholicOrNot: '', name: 'Corba', image: 'https://www.themealdb.com/images/media/meals/58oia61564916529.jpg' }]);
+  });
 
-    act(() => {
-      history.push('/done-recipes');
-    });
-    const doneRecipes = JSON.parse(localStorage.getItem('doneRecipes'));
-    const doneRecipesLength = doneRecipes.length;
-    const doneRecipesCards = await screen.findAllByTestId('recipe-card');
-    expect(doneRecipesCards.length).toBe(doneRecipesLength);
-    const recipeCard = await findByTestId('0-horizontal-favorite-btn');
-    userEvent.click(recipeCard);
-    const recipeCardAfterClick = await queryByTestId('0-horizontal-favorite-btn');
-    expect(recipeCardAfterClick).not.toBeInTheDocument();
+  it('Testa se a mensagem "No recipes done" é exibida, caso não tenha receitas feitas', () => {
+    renderWithRouter(<App />, route);
+    const noRecipes = screen.getByText('No recipes done');
+    expect(noRecipes).toBeInTheDocument();
   });
 });
